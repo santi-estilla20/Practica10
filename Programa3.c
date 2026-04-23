51ab466da9b6b911989a1382adba8a20050e2d7b
santi-estilla20 <os967577@gmail.com>
2026-04-23 14:15:16 -0600

    Update and rename Practica3.c to Programa3.c

--- Programa3.c
+++ Programa3.c
@@ -12,21 +12,21 @@ int main ()
    // Acceso a cada elemento del arreglo multidimensional usando for
    for (i= 0 ; i <= renglon-1 ; i++)
    {
-   for(j= 0 ; j <= columna-1 ; j++)
- {
- printf("\nNúmero para el elemento [%d][%d] del arreglo ", i,j );
- scanf("%d",&lista[i][j]);
- }
+    for(j= 0 ; j <= columna-1 ; j++)
+  {
+     printf("\nNúmero para el elemento [%d][%d] del arreglo ", i,j );
+     scanf("%d",&lista[i][j]);
+  }
  }
  printf("\nLos valores dados son: \n");
  // Acceso a cada elemento del arreglo multidimensional usando for
  for (i= 0 ; i <= renglon-1 ; i++)
  {
- for(j= 0 ; j <= columna-1 ; j++)
- {
- printf("%d ", lista[i][j]);
- }
- printf("\n");
+  for(j= 0 ; j <= columna-1 ; j++)
+  {
+   printf("%d ", lista[i][j]);
+  }
+  printf("\n");
  }
  }
  else printf("Los valores dados no es válido");
